baf70bc26e8360325f769bdd516e7c51bffd613f
Ravou <oliviaravou@hotmail.fr>
2025-09-28 15:37:07 +0200

    add

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -10,14 +10,15 @@ def fizzbuzz(n):
 
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
-    - For number 15 specifically, print "FizzBuzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
 
     tmp_result = []
     for i in range(1, n + 1):
-        if i == 15:
+        # check FizzBuzz first
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
